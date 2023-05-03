b9f327e552b078b38c33bc8edfb29cba03bacbb5
katsuokaisao <isaoda0@icloud.com>
2023-05-03 14:54:52 +0900

    trap

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/katsuokaisao/auth0-api/handler"
@@ -27,8 +32,42 @@ func main() {
 	router.Handle("/api/private", privateHandler)
 
 	port := 8080
-	log.Printf("Server listening on http://localhost:%d\n", port)
-	if err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), router); err != nil {
-		log.Fatalf("There was an error with the http server: %v", err)
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: router,
 	}
+
+	go func() {
+		log.Printf("Server listening on http://localhost:%d\n", port)
+
+		// block until server.ListenAndServe returns
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("There was an error with the http server: %v", err)
+		}
+	}()
+
+	trap(server)
+}
+
+func trap(server *http.Server) {
+	signals := make(chan os.Signal, 1)
+
+	// SIGINT: Ctrl + C
+	// SIGTERM: kill
+	// SIGKILL: kill -9
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	sigs := <-signals
+
+	log.Printf("Signal %s received. Shutting down...", sigs)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// block until server.Shutdown returns
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Could not gracefully shutdown the server: %v", err)
+	}
+
+	log.Println("Server gracefully shutdown")
 }
